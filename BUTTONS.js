a5698cbb19ec2994dd7c7c272e1d84ebe02c38b5
LF <leo.fanseda@ontariotechu.net>
2022-10-04 13:49:08 -0400

    transfering files

---
+++ BUTTONS.js
@@ -0,0 +1,15 @@
+function tabButton(event, name) {
+    var i, tabcontent, tablinks;
+    
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tabLinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+    document.getElementById(name).style.display = "block";
+    event.currentTarget.name += "active";
+} 
+// REFERENCED FROM W3 SCHOOLS
