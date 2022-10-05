41562ddaecaf2da71b330b9a82e16818d54739ea
LF <leo.fanseda@ontariotechu.net>
2022-10-04 22:18:04 -0400

    added comments

--- BUTTONS.js
+++ BUTTONS.js
@@ -1,14 +1,18 @@
+
+// CREATING THE FUNCTION FOR THE BUTTON
+// THIS FUNCTION HELPS DETERMINE WHEN A BUTTON IS ACTIVE
+// THIS IS NOT NECESSARY, I JUST WANTED TO MAKE SOMETHING IN JAVASCRIPT
 function tabButton(event, name) {
-    var i, tabcontent, tablinks;
     
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+    // CREATING THE VARIABLES NEEDED
+    var i, tablinks;
+    
+    // TAKES THE ACTIVE TAB AND REPLACES THE active CLASS WITH ''
     tablinks = document.getElementsByClassName("tabLinks");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace("active", "");
     }
+    // THE SELECTED TAB TAKES ON THE active CLASS
     document.getElementById(name).style.display = "block";
     event.currentTarget.name += "active";
 } 
